695c6158bf107aa81e81cbc52957b6ac6249181a
lepasq <pasquarelli.le@gmail.com>
2020-04-21 20:40:46 +0200

    Fix final bugs

--- api/menus.js
+++ api/menus.js
@@ -4,100 +4,109 @@ const menuItemsRouter = require("./menuItems.js");
 
 const sqlite3 = require("sqlite3");
 const db = new sqlite3.Database(
-  process.env.TEST_DATABASE || "./database.sqlite"
+    process.env.TEST_DATABASE || "./database.sqlite"
 );
 
 menusRouter.param("menuId", (req, res, next, menuId) => {
-  db.get(`SELECT * FROM Menu WHERE Menu.id = ${menuId}`, (err, menu) => {
-    if (err) {
-      next(err);
-    } else if (menu) {
-      req.menu = menu;
-      next();
-    } else {
-      res.sendStatus(404);
-    }
-  });
+    db.get(`SELECT * FROM Menu WHERE Menu.id = ${menuId}`, (err, menu) => {
+        if (err) {
+            next(err);
+        } else if (menu) {
+            req.menu = menu;
+            next();
+        } else {
+            res.sendStatus(404);
+        }
+    });
 });
 
 menusRouter.use("/:menuId/menu-items", menuItemsRouter);
 
 menusRouter.get("/:menuId", (req, res, next) => {
-  res.status(200).json({ menu: req.menu });
+    res.status(200).json({menu: req.menu});
 });
 
 menusRouter.put("/:menuId", (req, res, next) => {
-  const title = req.body.menu.title;
-  if (title) {
-    db.run(
-      `UPDATE Menu SET title = ${title} WHERE Menu.id = ${req.params.menuId}`,
-      (err) => {
-        if (err) {
-          next(err);
-        } else {
-          db.get(
-            `SELECT * FROM Menu WHERE Menu.id = ${req.params.menuId}`,
-            (err, menu) => {
-              res.status(200).json({ menu: menu });
+    const title = req.body.menu.title;
+    if (title) {
+        const sql = 'UPDATE Menu SET title = $title WHERE Menu.id = $menuId';
+        const values = {
+            $title: title,
+            $menuId: req.params.menuId
+        };
+
+        db.run(sql, values, (err) => {
+                if (err) {
+                    next(err);
+                } else {
+                    db.get(
+                        `SELECT * FROM Menu WHERE Menu.id = ${req.params.menuId}`,
+                        (err, menu) => {
+                            res.status(200).json({menu: menu});
+                        }
+                    );
+                }
             }
-          );
-        }
-      }
-    );
-  } else {
-    return res.sendStatus(400);
-  }
+        );
+    } else {
+        return res.sendStatus(400);
+    }
 });
 
 menusRouter.delete("/:menuId", (req, res, next) => {
-  const sql = `SELECT * FROM MenuItem WHERE MenuItem.menu_id = ${req.params.menuId}`;
-  db.get(sql, (err, menuItem) => {
-    if (err) {
-      next(err);
-    } else if (menuItem) {
-      // If the menu has menuitems it should not be deleted
-      return res.sendStatus(400);
-    } else {
-      const sql = `DELETE FROM Menu WHERE Menu.id = ${req.params.menuId}`;
-      db.run(sql, err => {
+    const sql = `SELECT * FROM MenuItem WHERE MenuItem.menu_id = ${req.params.menuId}`;
+    db.get(sql, (err, menuItem) => {
         if (err) {
-          next(err);
+            next(err);
+        } else if (menuItem) {
+            // If the menu has menuitems it should not be deleted
+            return res.sendStatus(400);
         } else {
-          res.sendStatus(204);
+            const sql = `DELETE FROM Menu WHERE Menu.id = ${req.params.menuId}`;
+            db.run(sql, err => {
+                if (err) {
+                    next(err);
+                } else {
+                    res.sendStatus(204);
+                }
+            });
         }
-      });
-    }
-  });
+    });
 });
 
 menusRouter.get("/", (req, res, next) => {
-  db.all("SELECT * FROM Menu", (err, menus) => {
-    if (err) {
-      next(err);
-    } else {
-      return res.status(200).json({ menus: menus });
-    }
-  });
+    db.all("SELECT * FROM Menu", (err, menus) => {
+        if (err) {
+            next(err);
+        } else {
+            return res.status(200).json({menus: menus});
+        }
+    });
 });
 
 menusRouter.post("/", (req, res, next) => {
-  const title = req.body.menu.title;
-  if (title) {
-    db.run(`INSERT INTO Menu (title) VALUES (${title})`, (err) => {
-      if (err) {
-        next(err);
-      } else {
-        db.get(
-          `SELECT * FROM Menu WHERE Menu.id = ${this.lastID}`,
-          (err, menu) => {
-            res.status(201).json({ menu: menu });
-          }
-        );
-      }
-    });
-  } else {
-    return res.sendStatus(400);
-  }
+    const title = req.body.menu.title;
+    if (title) {
+        const sql = 'INSERT INTO Menu (title) VALUES ($title)';
+        const values = {
+            $title: title
+        };
+
+        db.run(sql, values, function (err) {
+            if (err) {
+                next(err);
+            } else {
+                db.get(
+                    `SELECT * FROM Menu WHERE Menu.id = ${this.lastID}`,
+                    (err, menu) => {
+                        res.status(201).json({menu: menu});
+                    }
+                );
+            }
+        });
+    } else {
+        return res.sendStatus(400);
+    }
 });
 
 module.exports = menusRouter;
